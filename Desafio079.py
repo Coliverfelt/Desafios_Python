f5c266de73b3adb153c3e36e41d1a698bb09ae09
oliverfelt94@gmail.com <oliverfelt94@gmail.com>
2020-02-06 22:41:33 -0300

    Desafios 079-080

--- Desafio079.py
+++ Desafio079.py
@@ -1,13 +1,22 @@
 lista = list()
 while True:
     valor = int(input('Digite um valor: '))
-    if valor in lista:
-        valor = int(input('Esse número já existe na lista! Digite outro valor: '))
+    while valor in lista:
+        print('Esse número já existe na lista. Não poderei adicionar...')
+        continuar = str(input('Deseja continuar? [S/N] ')).lower().strip()[0]
+        while continuar not in 'sn':
+            print('Desculpe, não entendi! Digite apenas S ou N...')
+            continuar = str(input('Deseja continuar? [S/N] ')).lower().strip()[0]
+        if continuar in 'n':
+            break
+        if continuar in 's':
+            valor = int(input('Digite um valor: '))
     lista.append(valor)
-    print(f'Valor {valor} adicionado com sucesso!')
-    continuar = str(input('Deseja continuar? [S/N]')).lower().strip()[0]
+    print(f'Valor {valor} adicionado à lista com sucesso!')
+    continuar = str(input('Deseja continuar? [S/N] ')).lower().strip()[0]
     while continuar not in 'sn':
-        continuar = str(input('Por favor, digite apenas S ou N. Deseja continuar? [S/N]')).lower().strip()[0]
+        print('Desculpe, não entendi! Digite apenas S ou N...')
+        continuar = str(input('Deseja continuar? [S/N] ')).lower().strip()[0]
     if continuar in 'n':
         break
 lista.sort()
